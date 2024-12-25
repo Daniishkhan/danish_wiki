d6295e162d63e8be873c29665379cd4736f11031
Daniishkhan <danishafzalkhan@gmail.com>
2024-12-25 22:03:44 +0500

    added repo fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Default output directory
+    outDir: 'dist',
     emptyOutDir: true,
   },
-  base: '/', // Adjust this if deploying to a subdirectory
+  base: '/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': resolve(__dirname, './src'),
     },
   },
 });
